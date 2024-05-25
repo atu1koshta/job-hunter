51ed04fef007ecfc70ceb567e92a4f314a4e799e
atul-koshta <atulk2018@gmail.com>
2024-05-25 08:30:12 +0530

    Use service-account to edit spreadsheet

--- src/main/java/com/unemployed/service/GoogleSheetService.java
+++ src/main/java/com/unemployed/service/GoogleSheetService.java
@@ -2,15 +2,11 @@ package com.unemployed.service;
 
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.*;
@@ -18,7 +14,6 @@ import com.google.api.services.sheets.v4.model.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -29,11 +24,10 @@ import java.util.List;
 public class GoogleSheetService {
     private static final String APPLICATION_NAME = "Job Hunter";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
     private static final List<String> SCOPES =
             Collections.singletonList(SheetsScopes.SPREADSHEETS);
 
-    private static final String CREDENTIALS_FILE_PATH = "/client_secret_735628014392-pgr1nq7k682oe6cq2gbdktdpu36pcf3o.apps.googleusercontent.com.json";
+    private static final String SERVICE_ACCOUNT_CREDENTIALS_FILE_PATH = "/job-mailer-215e2e2d1f79.json";
 
     /**
      * Creates an authorized Credential object.
@@ -42,24 +36,17 @@ public class GoogleSheetService {
      * @return An authorized Credential object.
      * @throws IOException If the credentials.json file cannot be found.
      */
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
-            throws IOException {
-        // Load client secrets.
-        InputStream in = GoogleSheetService.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load service account credentials.
+        InputStream in = GoogleSheetService.class.getResourceAsStream(SERVICE_ACCOUNT_CREDENTIALS_FILE_PATH);
         if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+            throw new FileNotFoundException("Resource not found: " + SERVICE_ACCOUNT_CREDENTIALS_FILE_PATH);
         }
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        GoogleCredential credential = GoogleCredential.fromStream(in)
+                .createScoped(SCOPES);
+
+        return credential;
     }
 
     public static List<List<Object>> readSheet(String spreadsheetId, String range) {
